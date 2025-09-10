066e42958190b3d63ac2edb38bda944b40c2a524
EmilyMunozR <emilymunoz1018@gmail.com>
2025-09-10 03:38:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -108,10 +108,10 @@ def tbodyProductos():
     LIMIT 10 OFFSET 0
     """
 
+    return render_template("tbodyIntegrantes.html")
     cursor.execute(sql)
     registros = cursor.fetchall()
 
-    return render_template("tbodyIntegrantes.html", integrantes=registros)
 
 
 
@@ -267,6 +267,7 @@ def eliminarProducto():
     con.close()
 
     return make_response(jsonify({}))
+
 
 
 
