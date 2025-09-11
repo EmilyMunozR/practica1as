f477cc4802adaed9ef3785cde8de114678459356
Isaac hdz <haizesk05@gmail.com>
2025-09-11 13:32:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -668,7 +668,23 @@ def eliminarProyecto():
 
 
 #//////////////esta wea me trae una lista pal inerjoin //////////////////////////////////////////////////////////
+@app.route("/equipos/lista")
+def obtenerEquipos():
+    if not con.is_connected():
+        con.reconnect()
 
+    cursor = con.cursor(dictionary=True)
+    sql = """
+    SELECT idEquipo, nombreEquipo
+    FROM equipos
+    ORDER BY nombreEquipo ASC
+    """
+    
+    cursor.execute(sql)
+    registros = cursor.fetchall()
+    con.close()
+    
+    return make_response(jsonify(registros))
 #/////////////////////////////////////////////////////////
 
 
@@ -841,5 +857,6 @@ def eliminarProducto():
     con.close()
 
     return make_response(jsonify({}))
+
 
 
